e955049ba17a5e16095cb0efb292a7b597d4fadf
Pierre Lestringant <pierre.lestringant@gmail.com>
2020-11-13 09:52:17 +0100

    [Fix] incorrect offset in openssh pub key

--- searchKey/src/key_openssh.c
+++ searchKey/src/key_openssh.c
@@ -21,23 +21,23 @@ static int openssl_read_size(char* buffer, size_t* size_ptr, size_t* off_ptr, ui
 	return 0;
 }
 
-static void search_openssh_dss(char* buffer, size_t size, size_t offset, struct multiColumnPrinter* printer, const char* file_name){
+static void search_openssh_dss(struct fileChunk* chunk, size_t size, size_t offset, struct multiColumnPrinter* printer){
 	uint32_t item_size;
 
 	char* p;
 	size_t p_size;
-	size_t p_off;
+	off_t p_off;
 	char* q;
 	size_t q_size;
-	size_t q_off;
+	off_t q_off;
 	char* g;
 	size_t g_size;
-	size_t g_off;
+	off_t g_off;
 	char* u;
 	size_t u_size;
-	size_t u_off;
+	off_t u_off;
 
-	if (openssl_read_size(buffer, &size, &offset, &item_size)){
+	if (openssl_read_size(chunk->buffer, &size, &offset, &item_size)){
 		return;
 	}
 	if (item_size > size){
@@ -47,70 +47,70 @@ static void search_openssh_dss(char* buffer, size_t size, size_t offset, struct 
 	size -= item_size;
 	offset += item_size;
 
-	if (openssl_read_size(buffer, &size, &offset, &item_size)){
+	if (openssl_read_size(chunk->buffer, &size, &offset, &item_size)){
 		return;
 	}
 	if (!item_size || item_size > size){
 		return;
 	}
 
-	p = buffer + offset;
+	p = chunk->buffer + offset;
 	p_size = item_size;
-	p_off = offset;
+	p_off = offset + chunk->offset;
 
 	size -= item_size;
 	offset += item_size;
 
-	if (openssl_read_size(buffer, &size, &offset, &item_size)){
+	if (openssl_read_size(chunk->buffer, &size, &offset, &item_size)){
 		return;
 	}
 	if (!item_size || item_size > size){
 		return;
 	}
 
-	q = buffer + offset;
+	q = chunk->buffer + offset;
 	q_size = item_size;
-	q_off = offset;
+	q_off = offset + chunk->offset;
 
 	size -= item_size;
 	offset += item_size;
 
-	if (openssl_read_size(buffer, &size, &offset, &item_size)){
+	if (openssl_read_size(chunk->buffer, &size, &offset, &item_size)){
 		return;
 	}
 	if (!item_size || item_size > size){
 		return;
 	}
 
-	g = buffer + offset;
+	g = chunk->buffer + offset;
 	g_size = item_size;
-	g_off = offset;
+	g_off = offset + chunk->offset;
 
 	size -= item_size;
 	offset += item_size;
 
-	if (openssl_read_size(buffer, &size, &offset, &item_size)){
+	if (openssl_read_size(chunk->buffer, &size, &offset, &item_size)){
 		return;
 	}
 	if (!item_size || item_size > size){
 		return;
 	}
 
-	u = buffer + offset;
+	u = chunk->buffer + offset;
 	u_size = item_size;
-	u_off = offset;
+	u_off = offset + chunk->offset;
 
 	multiColumnPrinter_print_horizontal_separator(printer);
 
-	searchCryptoKey_report_success(p, p_size, p_off, _LITTLE_ENDIAN, "ssh-dss p", "pub", file_name, printer);
-	searchCryptoKey_report_success(q, q_size, q_off, _LITTLE_ENDIAN, "ssh-dss q", "pub", file_name, printer);
-	searchCryptoKey_report_success(g, g_size, g_off, _LITTLE_ENDIAN, "ssh-dss g", "pub", file_name, printer);
-	searchCryptoKey_report_success(u, u_size, u_off, _LITTLE_ENDIAN, "ssh-dss pub", "pub", file_name, printer);
+	searchCryptoKey_report_success(p, p_size, p_off, _LITTLE_ENDIAN, "ssh-dss p", "pub", chunk->file_name, printer);
+	searchCryptoKey_report_success(q, q_size, q_off, _LITTLE_ENDIAN, "ssh-dss q", "pub", chunk->file_name, printer);
+	searchCryptoKey_report_success(g, g_size, g_off, _LITTLE_ENDIAN, "ssh-dss g", "pub", chunk->file_name, printer);
+	searchCryptoKey_report_success(u, u_size, u_off, _LITTLE_ENDIAN, "ssh-dss pub", "pub", chunk->file_name, printer);
 
 	multiColumnPrinter_print_horizontal_separator(printer);
 }
 #else
-#define search_openssh_dss(buffer, size, offset, printer, file_name)
+#define search_openssh_dss(chunk, size, offset, printer)
 #endif
 
 #define OPENSSH_MIN_SIZE (8 + 7) // size + "ssh-dss" + size
@@ -126,7 +126,7 @@ void search_openssh_key(struct fileChunk* chunk, struct multiColumnPrinter* prin
 	for (i = 0; i < chunk->size - OPENSSH_MIN_SIZE + 1; i += 1){
 		name_size = __builtin_bswap32(*(uint32_t*)(chunk->buffer + i));
 		if (name_size == 7 && !memcmp(chunk->buffer + i + 4, "ssh-dss", 7)){
-			search_openssh_dss(chunk->buffer, chunk->size - i, i, printer, chunk->file_name);
+			search_openssh_dss(chunk, chunk->size - i, i, printer);
 		}
 	}
 }
